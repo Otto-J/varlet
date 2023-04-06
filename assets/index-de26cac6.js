986a1cd5ae5283eb12a93de50fe0e8a8338da49a
haoziqaq <357229046@qq.com>
2023-04-06 17:18:55 +0000

    Deploying to gh-page from @ varletjs/varlet@6b75dd5b0a628c673961f276a5a42b61df17abaa 🚀

---
+++ assets/index-de26cac6.js
@@ -0,0 +1,1 @@
+import{V as n}from"./Paper-825ab230.js";n.install=function(a){a.component(n.name,n)};
---
+++ assets/provide-e31e07f8.js
@@ -0,0 +1,1 @@
+import"./components-f50e3d1b.js";import{u as t}from"./useChildren-36e9566e.js";const I=Symbol("SWIPE_BIND_SWIPE_ITEM_KEY");function o(){const{childProviders:e,length:i,bindChildren:s}=t(I);return{length:i,swipeItems:e,bindSwipeItems:s}}export{I as S,o as u};
