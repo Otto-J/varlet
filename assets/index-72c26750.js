c283770a3608776c2869016db075d091c277afcf
haoziqaq <357229046@qq.com>
2023-05-11 03:37:28 +0000

    Deploying to gh-page from @ varletjs/varlet@fd5af009b82ba7d0cbd9c8cd16abf8d3451dae70 🚀

---
+++ assets/index-72c26750.js
@@ -0,0 +1,1 @@
+import{V as n}from"./Paper-f4e15e13.js";n.install=function(a){a.component(n.name,n)};
--- assets/index-b77cc8f0.js
+++
@@ -1,1 +0,0 @@
-import{V as n}from"./Paper-66184a3b.js";n.install=function(a){a.component(n.name,n)};
